e45266274f715eecc9d5b40beb5baa67a59abd70
ThonnyRG <zs21017265@estudiantes.uv.mx>
2025-12-03 17:11:44 -0600

    Updated description

--- apps/WindowsWeb/src/app/layout.tsx
+++ apps/WindowsWeb/src/app/layout.tsx
@@ -26,7 +26,7 @@ const segoe = localFont({
 export const metadata: Metadata = {
 	metadataBase: new URL('http://localhost:3000'),
 	title: 'Antonio Cruz Portfolio',
-	description: `Discover the elegance of Windows 11 in your browser! Powered by Next.js and Chakra UI, this open source project replicates the stylish design and smooth user interface of the real Windows 11. Explore familiar features and play around with the settings - all in your browser.`,
+	description: ``,
 	applicationName: 'Antonio Cruz Portfolio',
 	generator: 'Next.js',
 	creator: 'https://github.com/ThonnyRG',
@@ -50,7 +50,7 @@ export const metadata: Metadata = {
 		url: 'http://localhost:3000',
 		title: 'Antonio Cruz Portfolio',
 		siteName: 'Antonio Cruz Portfolio',
-		description: `Discover the elegance of Windows 11 in your browser! Powered by Next.js and Chakra UI, this open source project replicates the stylish design and smooth user interface of the real Windows 11. Explore familiar features and play around with the settings - all in your browser.`,
+		description: ``,
 	},
 	verification: {
 		google: process.env.GOOGLE_VERIFICATION,
